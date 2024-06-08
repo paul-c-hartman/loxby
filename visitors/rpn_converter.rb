3e4bc48c25e0ce8768724ddc609396f21771a379
Paul Hartman <paul.hartman@wallawalla.edu>
2024-06-08 01:37:29 -0700

    Implement Reverse Polish Notation converter using a visitor

---
+++ visitors/rpn_converter.rb
@@ -0,0 +1,23 @@
+require_relative 'base'
+
+class RPNConverter < Visitor
+  def print(expr)
+    expr.accept self
+  end
+
+  def visit_binary_expression(expr)
+    "#{expr.left.accept self} #{expr.right.accept self} #{expr.operator.lexeme}"
+  end
+
+  def visit_grouping_expression(expr)
+    expr.accept self
+  end
+
+  def visit_literal_expression(expr)
+    expr.value.to_s
+  end
+
+  def visit_unary_expression(expr)
+    "#{expr.right.accept self} #{expr.operator.lexeme}"
+  end
+end
